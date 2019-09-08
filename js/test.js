a9727d9e9181d77ea4603162676e93f81a686c96
liuyibao <734988964@qq.com>
2019-09-08 16:35:52 +0800

    update

---
+++ js/test.js
@@ -0,0 +1,10 @@
+var jq = document.createElement('script');
+jq.src = 'https://cdn.bootcss.com/jquery/3.4.1/jquery.js';
+document.getElementsByTagName('head')[0].appendChild(jq);
+
+function liuyibaoClick()
+{
+    jQuery("#Button2").click();
+}
+
+setTimeout("liuyibaoClick()", 900000);//15分钟
