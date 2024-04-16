02f5a556cb8ade3cc4fa08172cee6f3eec1bd0cd
liuyibao <734988964@qq.com>
2024-04-16 17:05:06 +0800

    update

--- game/frp_server_install.sh
+++ game/frp_server_install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# https://openpublic.oss-cn-shanghai.aliyuncs.com/gamestream/frp_server_install.sh
+
 cd ~
 wget https://openpublic.oss-cn-shanghai.aliyuncs.com/gamestream/frp_0.56.0_linux_amd64.tar.gz
 tar -zxvf frp_0.56.0_linux_amd64.tar.gz
